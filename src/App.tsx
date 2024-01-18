0b466a86b312266fefb5a8f1471ffdd518fec077
brobo1 <armanahz95@gmail.com>
2024-01-18 22:16:56 +0100

    Also, it cleans up unused variables and imports: a useEffect import and a navigate variable were removed as they were no longer needed in the App function.

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,13 @@ import {Home} from "./home/Home.tsx";
 //import Register from './assets/Register/Register'
 import LogIn from './logIn/logIn'
 import {BrowserRouter, Navigate, Route, Routes, useNavigate} from "react-router-dom";
-import React, {useEffect} from "react";
+import React from "react";
 import {Link} from "react-router-dom";
 import { Button } from '@mui/material';
 import {PlanetDetail} from "./examples/PlanetDetail.tsx";
 import {PlanetList} from "./examples/PlanetList.tsx";
 import SearchAppBar from "./home/Search.tsx";
 import Register from './Register/Register.tsx';
-import {DrawerMenu} from "./home/Drawer.tsx";
 
 
 type ProtectedRouteProps = {
@@ -20,7 +19,7 @@ type ProtectedRouteProps = {
 
 function ProtectedRoute(props: ProtectedRouteProps) {
 	const isLoggedIn = true;
-	const navigate = useNavigate();
+	//const navigate = useNavigate();
 
 		if (!isLoggedIn) {
 			return <Navigate to="/login"/>
@@ -52,6 +51,7 @@ function App() {
 				<Routes>
 					<Route path={"/"} element={<Home/>}></Route>
 					<Route path={"/login"} element={<ProtectedRoute><LogIn/></ProtectedRoute>}></Route>
+					<Route path={"/register"} element={<Register/>}></Route>
 					<Route path={"/planets"} element={<PlanetList/>}></Route>
 					<Route path={"/planets/:planetId"} element={<PlanetDetail/>}></Route>
 				</Routes>
