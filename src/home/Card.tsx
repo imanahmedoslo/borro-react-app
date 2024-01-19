ab849e514ac3cee97bb0b6d9981170fdc48c9f16
Sasibru <sander.simonsen.brustad@gmail.com>
2024-01-19 12:39:24 +0100

    Add ViewPost

--- src/home/Card.tsx
+++ src/home/Card.tsx
@@ -4,43 +4,10 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import {CardActionArea} from '@mui/material';
-import { useEffect, useState } from 'react';
 
-type postProps = {
-	id: number,
-	title: string,
-	image: string,
-	price: number,
-	dateFrom: Date,
-	dateTo: Date,
-	description: string,
-	location: string,
-	categoryId: number,
-	userId: number
-}
 
-async function FetchPost(props:postProps){
-	const res = await fetch(`https://localhost:7245/api/Post/${props.id}`);
-    try{
-        if(!res.ok) {
-            throw new Error(`Http error status code ${res.status}`);
-        }
 
-        const resObject = await res.json();
-        return resObject;
-    } catch (error) {
-        console.log(error);
-        return Error("Could not find find this post.");
-    }
-  }
-
-export default function ActionAreaCard(props: postProps) {
-	const [post, setPost] = useState<postProps>();
-
-	useEffect(() => {
-        FetchPost(props)
-        .then(post => setPost(post))
-    },[]);
+export default function ActionAreaCard() {
 
 	return (
 		<Card sx={{maxWidth: 345}} className={"CardMui"}>
