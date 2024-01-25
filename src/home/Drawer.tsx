5ecc44640b6b4c20a24ba6df90f0b29ad22ac0b9
imanahmedoslo <imanahmedimanahmed@gmail.com>
2024-01-25 14:03:21 +0100

    fix button colors and language , plus add createProfile button to drawer

--- src/home/Drawer.tsx
+++ src/home/Drawer.tsx
@@ -2,6 +2,7 @@ import {Button, Divider, Drawer, List, ListItem} from "@mui/material";
 import React from "react";
 import {Link} from "react-router-dom";
 import { useAuth } from "../App";
+import { useNavigate,Navigate } from "react-router-dom";
 
 export type DrawerMenuProps = {
   open: boolean;
@@ -10,6 +11,7 @@ export type DrawerMenuProps = {
 
 export function DrawerMenu(props: DrawerMenuProps) {
   const{sessionInfo}=useAuth()
+  const navigate=useNavigate()
 
   return <>
     <Drawer
@@ -30,7 +32,10 @@ export function DrawerMenu(props: DrawerMenuProps) {
         <Divider orientation="horizontal"/>
         <ListItem><Link to="/posts/:postId"><Button>Mine annonser</Button></Link></ListItem>
         <Divider orientation="horizontal"/>
-        <ListItem><Link to="/login"><Button>Meldinger</Button></Link></ListItem>
+        <ListItem><Link to="/"><Button>Meldinger</Button></Link></ListItem>
+        <ListItem><Button style={{backgroundColor:'#FBF7EF', border:'none', color:'#1976D2', boxShadow:'none', textAlign:'start', paddingLeft:'0px'}} variant="contained" onClick={() => navigate(`/createUserInfo/${sessionInfo?.id}`)}>
+        Lag bruker profil
+      </Button></ListItem>
         <Divider orientation="horizontal"/>
 
       </List>
